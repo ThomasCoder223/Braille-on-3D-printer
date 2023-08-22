326d6f2966744fe22f6d3a91f5129d1e0c43b202
DIY Dimentions <56737624+ThomasCoder223@users.noreply.github.com>
2023-08-22 18:22:29 +0200

    Add files via upload

--- Main.py
+++ Main.py
@@ -5,51 +5,53 @@ endXofset = 0
 import numbers
 
 def Translate(Row):
-  global gCode
+  global gCode    
   global endXofset
-  for item in Row:
+
+  for item in Row: 
     if item == '1':
-      gCode = gCode + 'G0 Z-5;\nG0 Z5;\n'
+      gCode = gCode + 'G0 Z-5;\nG0 Z5;\n'   # "1" --> Press down
     elif item == '2':
-      gCode = gCode + 'G0 X2;\n'
-      endXofset = endXofset + 2
+      gCode = gCode + 'G0 X2;\n'            # "2" --> Move right 2 mm
+      endXofset = endXofset + 2    # Add 2 to line offset
     elif item == '4':
-      gCode = gCode + 'G0 X4;\n'
-      endXofset = endXofset + 4
+      gCode = gCode + 'G0 X4;\n'            # "4" --> Move right 4 mm
+      endXofset = endXofset + 4    # Add 4 to line offset
     elif item == '0':
-      gCode = gCode + ''
+      gCode = gCode + ''                    # "0" --> Nothing
     else:
       gCode = gCode + ';error\n'
 
-def Append():
+def Append():   # For each row, add the two corresponding digits
   if len(nxt) == 6:
-      firstRow.append(nxt[0])
+      firstRow.append(nxt[0])   # Two top digits
       firstRow.append('2')
       firstRow.append(nxt[3])
       firstRow.append('4')
 
-      secondRow.append(nxt[1])
+      secondRow.append(nxt[1])    # Two middle digits
       secondRow.append('2')
       secondRow.append(nxt[4])
       secondRow.append('4')
 
-      thirdRow.append(nxt[2])
+      thirdRow.append(nxt[2])   # Two bottom digits
       thirdRow.append('2')
       thirdRow.append(nxt[5])
       thirdRow.append('4')
 
-  elif len(nxt) == 12:
+  else:                         # Here we have double the number of digits so as to take into ccount the CAPITAL letter symbols
      firstRow.append(nxt[0])
      firstRow.append('2')
-     firstRow.append(nxt[3])
+     firstRow.append(nxt[3])    # Four top digits
      firstRow.append('4')
      firstRow.append(nxt[6])
      firstRow.append('2')
      firstRow.append(nxt[9])
      firstRow.append('4')
+
      secondRow.append(nxt[1])
      secondRow.append('2')
-     secondRow.append(nxt[4])
+     secondRow.append(nxt[4])   # Four middle digits
      secondRow.append('4')
      secondRow.append(nxt[7])
      secondRow.append('2')
@@ -58,69 +60,32 @@ def Append():
 
      thirdRow.append(nxt[2])
      thirdRow.append('2')
-     thirdRow.append(nxt[5])
+     thirdRow.append(nxt[5])   # Four bottom digits
      thirdRow.append('4')
      thirdRow.append(nxt[8])
      thirdRow.append('2')
      thirdRow.append(nxt[11])
      thirdRow.append('4')
 
-  else:
-     firstRow.append(nxt[0])
-     firstRow.append('2')
-     firstRow.append(nxt[3])
-     firstRow.append('4')
-     firstRow.append(nxt[6])
-     firstRow.append('2')
-     firstRow.append(nxt[9])
-     firstRow.append('4')
-     firstRow.append(nxt[12])
-     firstRow.append('2')
-     firstRow.append(nxt[15])
-     firstRow.append('4')
 
-     secondRow.append(nxt[1])
-     secondRow.append('2')
-     secondRow.append(nxt[4])
-     secondRow.append('4')
-     secondRow.append(nxt[7])
-     secondRow.append('2')
-     secondRow.append(nxt[10])
-     secondRow.append('4')
-     secondRow.append(nxt[13])
-     secondRow.append('2')
-     secondRow.append(nxt[16])
-     secondRow.append('4')
-
-     thirdRow.append(nxt[2])
-     thirdRow.append('2')
-     thirdRow.append(nxt[5])
-     thirdRow.append('4')
-     thirdRow.append(nxt[8])
-     thirdRow.append('2')
-     thirdRow.append(nxt[11])
-     thirdRow.append('4')
-     thirdRow.append(nxt[14])
-     thirdRow.append('2')
-     thirdRow.append(nxt[17])
-     thirdRow.append('4')
+#--------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------
 
 
-
-xStart = int(input('Enter X start position under 235\n'))
+xStart = int(input('Enter X start position under 235\n'))   # Algorithm starts here 
 while xStart > 235:
   print('value over 235, chose smaller number')
-  xStart = int(input('Enter new X start position.\n'))
+  xStart = int(input('Enter new X start position.\n'))    # Get start location for X axe 
 print('\n')
 
 yStart = int(input('Enter Y start position under 235.\n'))
 while yStart > 235:
   print('value over 235, chose smaller number')
-  yStart = int(input('Enter new Y start position.\n'))
+  yStart = int(input('Enter new Y start position.\n'))    # Get start location for Y axe
 print('\n')
 
 
-gCode = 'G90;\nG0 Z5;\nG0 X'+ str(xStart) + ' Y' + str(yStart) + ';\n' + 'G91;\n'
+gCode = 'G90;\nG0 Z5;\nG0 X'+ str(xStart) + ' Y' + str(yStart) + ';\n' + 'G91;\n'   # Add start location instructions to G-Code
 
 text = input('Enter what you want to type.\n')
 
@@ -129,26 +94,26 @@ numbers = []
 nbrOn = False
 
 for letter in text:
-  listOfCharacters.append(letter)
+  listOfCharacters.append(letter)   # Create an array with every character
 
-for i in range(len(listOfCharacters)):
-  if listOfCharacters[i].isdigit():
+for i in range(len(listOfCharacters)):    # Create an array with only the digits
+  if listOfCharacters[i].isdigit(): 
     numbers.append(i)
 
-for i in range(len(listOfCharacters)):
+for i in range(len(listOfCharacters)):    # For each character
 
-  if i in numbers:
-    if nbrOn == False:
-      nxt = '001111'
+  if i in numbers:    
+    if nbrOn == False:    # If it is in the array of numbers and the previous character wasn't, add the number symbol
+      nxt = '001111'    
       Append()
       nbrOn = True
   else:
-    if nbrOn == True:
+    if nbrOn == True:   # If it isn't in the array of numbers but the previous character was, add the 'back to letters' symbol
       nxt = '000011'
       Append()
       nbrOn = False
 
-  match listOfCharacters[i]:
+  match listOfCharacters[i]:    # Use the dictionnary to find which 6-digit code the character corresponds to
     case " ":
       nxt = '000000'
     case "a":
@@ -298,32 +263,34 @@ for i in range(len(listOfCharacters)):
     case "0":
       nxt = '010110'
     case _:
-      print('i dont know that one yet')
+      print('ERROR ---> I dont know that one yet.')   
 
-  Append()  
+  Append()    # See function 'Append()'
+
+
+
+
+gCode = gCode + ';First Row\n'
+Translate(reversed(firstRow))   # See function 'Translate()' 
+gCode = gCode + 'G0 X-' + str(endXofset) + ' Y-2;\n'    # Tell the 3-D printer to go to the next row, goes down 2 mm and back the length of the previous line
+endXofset = 0
+
+gCode = gCode + ';Second Row\n'
+Translate(reversed(secondRow))
+gCode = gCode + 'G0 X-' + str(endXofset) + ' Y-2;\n'    # Tell the 3-D printer to go to the next row, goes down 2 mm and back the length of the previous line
+endXofset = 0
+
+gCode = gCode + ';Third Row\n'
+Translate(reversed(thirdRow))
+gCode = gCode + 'G0 X-' + str(endXofset) + ' Y-2;\n'    # Tell the 3-D printer to go to the next row, goes down 2 mm and back the length of the previous line
+endXofset = 0
 
 print(firstRow)
 print(secondRow)
 print(thirdRow)
-print("G-Code Generated !")
+print("G-Code Generated !")   # Just some completion confirmation
 
 
-gCode = gCode + ';First Row\n'
-Translate(firstRow)
-gCode = gCode + 'G0 X-' + str(endXofset) + ' Y-2;\n'
-endXofset = 0
-
-gCode = gCode + ';Second Row\n'
-Translate(secondRow)
-gCode = gCode + 'G0 X-' + str(endXofset) + ' Y-2;\n'
-endXofset = 0
-
-gCode = gCode + ';Third Row\n'
-Translate(thirdRow)
-gCode = gCode + 'G0 X-' + str(endXofset) + ' Y-2;\n'
-endXofset = 0
-
-
-text_file = open(r'Export.gcode', 'w')
-text_file.write(gCode)
-text_file.close()
+text_file = open(r'Export.gcode', 'w')    # Create a text file
+text_file.write(gCode)                    # Add the G-Code we generated
+text_file.close()                         # Save and close
